972eb2477ecd72d466ba3ce04f62c511ae9eb4e9
stelian <stelian@futureworkshops.com>
2016-07-19 16:41:59 +0100

    Update migrations and db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708154820) do
+ActiveRecord::Schema.define(version: 20160719152449) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,8 +38,9 @@ ActiveRecord::Schema.define(version: 20160708154820) do
   create_table "videos", force: :cascade do |t|
     t.string   "name"
     t.string   "youtube_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
