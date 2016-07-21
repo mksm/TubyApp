b3a4f0f202f1f1140603e8ae064750c3f37b5b98
stelian <stelian@futureworkshops.com>
2016-07-21 12:03:08 +0100

    feature : #124554741, define factory and spec to check association between video and category

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :video do
-    name "MyString"
-    youtube_id "MyString"
+    sequence(:name) { |n| "Video#{n}" }
+    sequence(:youtube_id) { |n| "youtubeid#{n}" }
+    category
   end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+        create(:video).should be_valid
+  end
+
+  it "is invalid without a video name"
+  it "is invalid without a youtube id"
+
 end
