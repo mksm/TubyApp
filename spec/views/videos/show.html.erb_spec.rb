c57f3cfa68736227aaa979446a6918e816ef56b0
stelian <stelian@futureworkshops.com>
2016-07-22 10:38:34 +0100

    feature : #123840691, show category on video details page + rspec

--- spec/views/videos/show.html.erb_spec.rb
+++ spec/views/videos/show.html.erb_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "videos/show", type: :view do
   before(:each) do
     @video = assign(:video, Video.create!(
-      :name => "Name",
-      :youtube_id => "Youtube"
+        :name => "Name",
+        :youtube_id => "Youtube",
+        :category => Category.create!(:name =>"Category")
     ))
   end
 
@@ -12,5 +13,6 @@ RSpec.describe "videos/show", type: :view do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/Youtube/)
+    expect(rendered).to match(/Category/)
   end
 end
