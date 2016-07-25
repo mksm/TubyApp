795925340cc4cfbd24bd6bb6eb2f1b7aa0fef00b
Matt Brooke-Smith <matt@futureworkshops.com>
2016-07-25 17:16:16 +0100

    Update api/base_controller
    Recover from cancancan errors gracefully

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,7 @@
 class Api::BaseController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    render :status => 403
+  end
 end
