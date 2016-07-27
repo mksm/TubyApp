cf62d7b93a20caa64488e2d008c9d826144ee8fc
Matt Brooke-Smith <matt@futureworkshops.com>
2016-07-27 13:31:03 +0100

    Fix video validation

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :category
-  validates :name, length: { minimum: 3 },
-            presence: true
-  validates :youtube_id, length: { minimum: 5 },
-            presence: true
+  
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :youtube_id, length: { minimum: 5 }, presence: true
+  validates :category, presence: true
 end
