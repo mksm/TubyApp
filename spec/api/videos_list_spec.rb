6107071d29550d5283d53843c9953f21f720195c
Matt Brooke-Smith <matt@futureworkshops.com>
2016-08-10 12:48:16 +0100

    spec

--- spec/api/videos_list_spec.rb
+++ spec/api/videos_list_spec.rb
@@ -52,4 +52,37 @@ feature 'videos list' do
     it { expect(response.body).to have_json_type(String).at_path("0/created_at") }  
   end
   
+  context "single video specifying partial name" do
+    let(:c1) { create :category }
+    let!(:v1) { create(:video, name: "Mickey Mouse", youtube_id: "abc1234", category: c1) }
+    let!(:v2) { create(:video, name: "Donald", youtube_id: "zxy1234", category: c1) }
+    
+    let(:category_ids) { [c1.id] }
+    
+    before(:each) { get "/api/videos.json", params: {q: "ickey", category_ids: category_ids.join(",")} }
+    
+    it { expect(response.status).to eq 200 }
+    
+    it { expect(response.body).to have_json_size(1).at_path("/") }    
+
+    it { expect(parse_json(response.body, "0/id")).to eq v1.id }
+    it { expect(parse_json(response.body, "0/name")).to eq "Mickey Mouse" }
+    it { expect(parse_json(response.body, "0/youtube_id")).to eq "abc1234" }
+    it { expect(parse_json(response.body, "0/category_id")).to eq c1.id }    
+    it { expect(response.body).to have_json_type(String).at_path("0/created_at") }    
+  end
+  
+  context "no videos specifying partial name" do
+    let(:c1) { create :category }
+    let!(:v1) { create(:video, name: "Mickey Mouse", youtube_id: "abc1234", category: c1) }
+    let!(:v2) { create(:video, name: "Donald", youtube_id: "zxy1234", category: c1) }
+    
+    let(:category_ids) { [c1.id] }
+    
+    before(:each) { get "/api/videos.json", params: {q: "Stelian", category_ids: category_ids.join(",")} }
+    
+    it { expect(response.status).to eq 200 }
+    it { expect(response.body).to have_json_size(0).at_path("/") }    
+  end
+  
 end
