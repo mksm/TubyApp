773070c1a2d61a782178562baeeabe3e51c4abb0
stelian <stelian@futureworkshops.com>
2016-08-16 10:15:12 +0100

    chore : #127979517, make videos default page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :categories
   resources :videos
   devise_for :users, :controllers => { :sessions => 'sessions' }
-  
+
+  root :to => 'videos#index'
+
   namespace 'api' do
     resources :categories, only: :index
     resources :videos, only: :index        
