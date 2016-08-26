8a54e699367de62130b9794e043eaaa92b21ea2c
Matt Brooke-Smith <matt@futureworkshops.com>
2016-08-26 13:02:12 +0100

    128057787 unique youtube ids on video

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,6 @@ class Video < ApplicationRecord
   belongs_to :category
   
   validates :name, length: { minimum: 3 }, presence: true
-  validates :youtube_id, length: { minimum: 5 }, presence: true
+  validates :youtube_id, length: { minimum: 5 }, presence: true, uniqueness: true
   validates :category, presence: true
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -13,5 +13,21 @@ RSpec.describe Video, type: :model do
   it "is invalid without a category" do
     expect(build(:video,category: nil)).not_to be_valid
   end
-
+  
+  describe "validations" do
+    context "destroy dependent" do
+      let!(:category) { create(:category, :videos => create_list(:video, 1)) }
+      
+      before(:each) { Category.first.destroy!; }
+      
+      xit { expect(Category.count).to eq 0 }
+      xit { expect(Video.count).to eq 0 }
+    end
+  end
+  
+  context "a youtube id already exists" do
+    let!(:v1) { create(:video, youtube_id: "abc123") } 
+    
+    it { expect( build(:video, youtube_id: "abc123") ).not_to be_valid }   
+  end
 end
