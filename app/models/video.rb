ee93aaacf07a9e312e39182b3ce3fe34493aaf52
Matt Brooke-Smith <matt@futureworkshops.com>
2016-08-26 13:52:02 +0100

    Update video
    Add import_csv

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,16 @@
+require 'csv'    
+
 class Video < ApplicationRecord
   belongs_to :category
   
   validates :name, length: { minimum: 3 }, presence: true
   validates :youtube_id, length: { minimum: 5 }, presence: true, uniqueness: true
   validates :category, presence: true
+  
+  def self.import_csv(csv_text)
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |row|
+      Video.create!(row.to_hash)
+    end
+  end
 end
