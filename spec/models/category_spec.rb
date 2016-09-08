4c9ac0b59715b0cdc7f1521581d422684bf4b194
Matt Brooke-Smith <matt@futureworkshops.com>
2016-09-08 13:41:10 +0100

    spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -12,6 +12,20 @@ RSpec.describe Category, type: :model do
       xit { expect(Video.count).to eq 0 }
     end
   end
+  
+  describe "#category_image_url" do
+    context "no videos" do
+      subject!(:category) { create(:category, videos: []) }
+      it { expect(subject.category_image_url).to be nil }
+    end
+    
+    context "single video" do
+      subject!(:category) { create(:category) }
+      let!(:video) { create(:video, youtube_id: "abc123", category: category)}
+      it { expect(subject.category_image_url).to eq "http://img.youtube.com/vi/abc123/0.jpg" }
+    end
+  end
+
 
   it "is invalid without a name"  do
     expect( build(:category, name: nil) ).not_to be_valid
