34699fb315c2c1f57adc93e6df6424fded48b1c8
Matt Brooke-Smith <matt@futureworkshops.com>
2016-10-07 10:55:06 +0100

    Update controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,35 +22,6 @@ class CategoriesController < ApplicationController
     redirect_to categories_path, notice: 'Category was successfully destroyed.'
   end
 
-  def notify
-    return unless ENV['NOTIFIABLE_URL']
-    message = 'We\'ve added some new categories'
-
-    Rails.logger.debug "Sending notification : #{message}"
-
-    http = Net::HTTP.new(ENV['NOTIFIABLE_URL'])
-
-    request = Net::HTTP::Post.new("/notifications_api/v1/notifications")
-    request["Authorization"] = ENV['NOTIFIABLE_ACCESS_ID']
-    request["Accept"] = "application/json"
-
-    data = {
-        "app_id" => ENV['NOTIFIABLE_APP_ID'],
-        "notification[localized_notifications_attributes][][message]" => message,
-        "notification[localized_notifications_attributes][][locale]" => "en" }
-
-    request.set_form_data(data)
-
-    Rails.logger.debug "Notification response: #{response.code}"
-
-    if response.code == 200
-      redirect_to categories_path, notice: 'Notification has been sent.'
-    else
-      redirect_to categories_path, notice: 'Notification was not sent.'
-    end
-
-  end
-
   private
     def category_params
       params.require(:category).permit(:name_en, :name_ar)
