b30f2e349da51e94885521ddb43f0e25e9705861
develaper <lachguer.brahim@gmail.com>
2017-03-27 14:05:34 +0100

    [feature:142109931]Changes channels index to fit the previous index style

--- spec/views/channels/index.html.erb_spec.rb
+++ spec/views/channels/index.html.erb_spec.rb
@@ -4,19 +4,33 @@ RSpec.describe "channels/index", type: :view do
   before(:each) do
     assign(:channels, [
       Channel.create!(
-        :name => "Name",
-        :youtube_id => "Youtube"
+        :name => "Name1",
+        :youtube_id => "Youtube1"
       ),
       Channel.create!(
-        :name => "Name",
-        :youtube_id => "Youtube"
+        :name => "Name2",
+        :youtube_id => "Youtube2"
       )
     ])
   end
 
   it "renders a list of channels" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Youtube".to_s, :count => 2
+    assert_select "tr>td", :text => "Name1".to_s, :count => 1
+    assert_select "tr>td", :text => "Youtube1".to_s, :count => 1
+    assert_select "tr>td", :text => "Name2".to_s, :count => 1
+    assert_select "tr>td", :text => "Youtube2".to_s, :count => 1
+  end
+  it "renders a link to each channel edit form" do
+    firstId = Channel.find_by(name:"Name1").id
+    secondId = Channel.find_by(name:"Name2").id
+    render
+    assert_select "tr>td>a[href=?]", "/channels/#{firstId}/edit"
+    assert_select "tr>td>a[href=?]", "/channels/#{secondId}/edit"
+  end
+  it "renders a new channel button" do
+    render
+    assert_select "a", :text => "New Channel".to_s, :count => 1
+    assert_select "a[href=?]", "/channels/new"
   end
 end
