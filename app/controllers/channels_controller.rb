90f7b5a9d6896fe4022f0922cecc589584b860ea
develaper <lachguer.brahim@gmail.com>
2017-03-28 18:03:41 +0100

    [feature:142109931]Removes show from route and controller

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,14 +2,8 @@ class ChannelsController < ApplicationController
   before_action :set_channel, only: [:show, :edit, :update, :destroy]
 
   # GET /channels
-  # GET /channels.json
   def index
     @channels = Channel.all
-  end
-
-  # GET /channels/1
-  # GET /channels/1.json
-  def show
   end
 
   # GET /channels/new
@@ -22,43 +16,28 @@ class ChannelsController < ApplicationController
   end
 
   # POST /channels
-  # POST /channels.json
   def create
     @channel = Channel.new(channel_params)
-
-    respond_to do |format|
-      if @channel.save
-        format.html { redirect_to @channel, notice: 'Channel was successfully created.' }
-        format.json { render :show, status: :created, location: @channel }
-      else
-        format.html { render :new }
-        format.json { render json: @channel.errors, status: :unprocessable_entity }
-      end
+    if @channel.save
+      redirect_to channels_url, notice: 'Channel was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /channels/1
-  # PATCH/PUT /channels/1.json
   def update
-    respond_to do |format|
-      if @channel.update(channel_params)
-        format.html { redirect_to @channel, notice: 'Channel was successfully updated.' }
-        format.json { render :show, status: :ok, location: @channel }
-      else
-        format.html { render :edit }
-        format.json { render json: @channel.errors, status: :unprocessable_entity }
-      end
+    if @channel.update(channel_params)
+      redirect_to channels_url, notice: 'Channel was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /channels/1
-  # DELETE /channels/1.json
   def destroy
     @channel.destroy
-    respond_to do |format|
-      format.html { redirect_to channels_url, notice: 'Channel was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to channels_url, notice: 'Channel was successfully destroyed.'
   end
 
   private
