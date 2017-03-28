6f446c4b259849688848924b175d493e17763ce6
develaper <lachguer.brahim@gmail.com>
2017-03-28 18:03:41 +0100

    [feature:142109931]Removes show from route and controller

--- app/views/channels/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @channel, :id, :name, :youtube_id, :created_at, :updated_at
--- spec/factories/channels.rb
+++ spec/factories/channels.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :channel do
-    sequence(:name) {|n| "Cannel#{n}" }
+    sequence(:name_en) {|n| "Cannel#{n}" }
+    sequence(:name_ar) {|n| "#{n}اسم" }
     sequence(:youtube_id) {|n| "youtubeid#{n}" }
   end
 end
