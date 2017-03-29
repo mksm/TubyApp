30417bc27466415f80b871165fefc24222c53e05
fwibrahim <ibrahim@futureworkshops.com>
2017-03-29 17:53:02 +0100

    [feature:142109931]Fixs yt validation for testing

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,17 +1,18 @@
+class YoutubeChannelValidator < ActiveModel::Validator
+  def validate(record)
+    return true if Rails.env.test?
+    yt_channel = Yt::Channel.new id: record.id
+    yt_channel.video_count
+  end
+end
+
 class Channel < ApplicationRecord
 
   validates :name, length: { minimum: 3 }, presence: true
   validates :youtube_id, length: { minimum: 5 }, presence: true, uniqueness: true
-
-  before_create :validate_existence_of_yt_channel
-  before_update :validate_existence_of_yt_channel
+  validates_with YoutubeChannelValidator
 
   translates :name
   globalize_accessors
 
-  private
-    def validate_existence_of_yt_channel
-      yt_channel = Yt::Channel.new id: id
-      yt_channel.video_count
-    end
 end
