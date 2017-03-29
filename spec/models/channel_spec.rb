4b2ecbf97407d984c3dff1791eaca141e3ceea1b
fwibrahim <ibrahim@futureworkshops.com>
2017-03-29 11:24:50 +0100

    [feature:142109931]Removes and cleans some tests

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Channel, type: :model do
   it "has a valid factory" do
-    FactoryGirl.create(:channel).should be_valid
+    expect( FactoryGirl.create(:channel) ).to be_valid
   end
   it "is invalid without a channel name" do
     expect( build(:channel, name: nil) ).not_to be_valid
