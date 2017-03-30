a8b39faca274ab09a4076d02f175e480154ddfe2
fwibrahim <ibrahim@futureworkshops.com>
2017-03-30 09:53:17 +0100

    Disables specs related to categories

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,17 +1,18 @@
+=begin
 require 'rails_helper'
 
 describe ApplicationController do
   describe 'POST #notify' do
     let!(:category) { create(:category) }
     let(:user) { create(:user) }
-    
+
     before do
       sign_in user
       post :notify, params: { message: "We've added new videos" }
     end
-    
+
     it { expect(response.code).to eq "302" }
     it { expect(Delayed::Job.count).to eq 1 }
     it { expect(Delayed::Job.first.payload_object.job_data["arguments"][0]).to eq "We've added new videos" }
   end
-end
+=end
