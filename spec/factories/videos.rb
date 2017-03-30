0a667d5b23499e8e0a6daa4a21da96003ce8b945
fwibrahim <ibrahim@futureworkshops.com>
2017-03-30 13:37:34 +0100

    [feature:142109931]Fix minimum length in the video factory to pass validation

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :video do |f|
     f.name { Faker::Lorem.sentence }
-    f.youtube_id { Faker::Internet.password(5,10) }
+    f.youtube_id { Faker::Internet.password(8,10) }
     channel
   end
 end
