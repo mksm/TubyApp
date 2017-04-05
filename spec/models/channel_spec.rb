490155c1d365bc3d9444ed7ac87a922a87b86d51
fwibrahim <ibrahim@futureworkshops.com>
2017-04-05 12:43:40 +0200

    [feature:142109931]Adds test for videos_count

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Channel, type: :model do
   it "is invalid without a youtube_id" do
     expect( build(:channel, youtube_id: nil) ).not_to be_valid
   end
+  it "has a valid videos counter method" do
+    channel = FactoryGirl.create(:channel)
+    first_video = FactoryGirl.create(:video, channel_id: channel.id)
+    second_video = FactoryGirl.create(:video, channel_id: channel.id)
+    expect(channel.videos_count).to eq(2)
+  end
+  it "videos are not counted after destroy" do
+    channel = FactoryGirl.create(:channel)
+    first_video = FactoryGirl.create(:video, channel_id: channel.id)
+    second_video = FactoryGirl.create(:video, channel_id: channel.id)
+    second_video.destroy
+    expect(channel.videos_count).to eq(1)
+  end
 end
