c1679df37dfefe86d785d45d6bf4a25a74067ee7
fwibrahim <ibrahim@futureworkshops.com>
2017-04-06 13:15:31 +0200

    [feature:142109931]Restores custom validation to model file

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,16 @@
+class YoutubeChannelValidator < ActiveModel::Validator
+
+  def validate(record)
+    if Rails.env.test? || Rails.env.development?
+      return true
+    end
+    yt_channel = Yt::Channel.new id: record.youtube_id
+    yt_channel.video_count
+  end
+end
 class Channel < ApplicationRecord
   has_many :videos, dependent: :destroy
-  
+
   validates :name, length: { minimum: 3 }, presence: true
   validates :youtube_id, length: { minimum: 5 }, presence: true, uniqueness: true
   validates_with YoutubeChannelValidator
