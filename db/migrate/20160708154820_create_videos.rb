d8d87bfe0bf19620677f713c7532659163854555
fwibrahim <ibrahim@futureworkshops.com>
2017-04-06 17:30:14 +0200

    [feature:142109931]Fixs lack of channel_id attribute in video

--- db/migrate/20160708154820_create_videos.rb
+++ db/migrate/20160708154820_create_videos.rb
@@ -3,19 +3,20 @@ class CreateVideos < ActiveRecord::Migration[5.0]
     create_table :videos do |t|
       t.string :name
       t.string :youtube_id
+      t.integer :channel_id
 
       t.timestamps
     end
-    
+
     reversible do |dir|
       dir.up do
         Video.create_translation_table! name: :string
       end
 
-      dir.down do 
+      dir.down do
         Video.drop_translation_table!
       end
     end
-    
+
   end
 end
