d2e7ebee6b84d3d27ab2ec2645a9109f006136d7
fw.matt <matt@futureworkshops.com>
2017-04-06 18:29:30 +0100

    Update channel_spec #name tests
    Move to describe style

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe Channel, type: :model do
   it "has a valid factory" do
     expect( FactoryGirl.create(:channel) ).to be_valid
   end
-  it "is invalid without a channel name" do
-    expect( build(:channel, name: nil) ).not_to be_valid
+  
+  describe '#name' do
+    context 'invalid' do
+      subject { build(:channel, name: nil) }
+      it { expect(subject).to_not be_valid }
+    end
   end
   it "is invalid without a youtube_id" do
     expect( build(:channel, youtube_id: nil) ).not_to be_valid
