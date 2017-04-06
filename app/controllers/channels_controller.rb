8a9b90f7e18c9ac37724683cc7275a1d3d0c3331
fw.matt <matt@futureworkshops.com>
2017-04-06 18:20:18 +0100

    Refactor Channel youtube validations
    * Move the validation into a method (I think it works better this way)
    * rescue errors inside the validator & add errors to the model
    * Dry up the controller, removing duplicated rescues and redirect
    * Dry up controller by generating a generic error message

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,36 +2,18 @@ class ChannelsController < ApplicationController
   load_and_authorize_resource
 
   def create
-    begin
     if @channel.save
       redirect_to channels_url, notice: 'Channel was successfully created.'
     else
-      flash[:alert] = "There has been an error in saving proccess. Please try again."
-      render :new
-    end
-    rescue Yt::Errors::NoItems => e
-      flash[:alert] = "There is an error in the channel's youtube id. Please correct it before saving."
-      render :new
-    rescue => e
-      flash[:alert] = "There has been an error in the saving proccess. Please try again."
-      render :new
+      render_with_alert :new
     end
   end
 
   def update
-    begin
     if @channel.update(channel_params)
       redirect_to channels_url, notice: 'Channel was successfully updated.'
     else
-      flash[:alert] = "There has been an error in the saving proccess. Please try again."
-      render :edit
-    end
-    rescue Yt::Errors::NoItems => e
-      flash[:alert] = "There is an error in the channel's youtube id. Please correct it before saving."
-      render :edit
-    rescue => e
-      flash[:alert] = "There has been an error in the saving proccess. Please try again."
-      render :edit
+      render_with_alert :edit
     end
   end
 
@@ -43,5 +25,10 @@ class ChannelsController < ApplicationController
   private
   def channel_params
     params.require(:channel).permit(:name_en, :name_ar, :youtube_id)
+  end
+  
+  def render_with_alert(template)
+    flash[:alert] = @channel.errors.full_messages.to_sentence
+    render template
   end
 end
--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,19 +1,9 @@
-class YoutubeChannelValidator < ActiveModel::Validator
-
-  def validate(record)
-    if Rails.env.test? || Rails.env.development?
-      return true
-    end
-    yt_channel = Yt::Channel.new id: record.youtube_id
-    yt_channel.video_count
-  end
-end
 class Channel < ApplicationRecord
   has_many :videos, dependent: :destroy
 
   validates :name, length: { minimum: 3 }, presence: true
   validates :youtube_id, length: { minimum: 5 }, presence: true, uniqueness: true
-  validates_with YoutubeChannelValidator
+  validate :youtube_id_is_valid_on_youtube
 
   translates :name
   globalize_accessors
@@ -21,5 +11,15 @@ class Channel < ApplicationRecord
   def videos_count
     videos.count
   end
-
+  
+  private
+  def youtube_id_is_valid_on_youtube
+    return if Rails.env.test? || Rails.env.development?
+    
+    begin
+      Yt::Channel.new(id: youtube_id).video_count
+    rescue Yt::Errors::NoItems => e
+      errors.add(:youtube_id, "can't be found")
+    end
+  end
 end
