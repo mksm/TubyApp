6bab066846314006d9b1803b968a27532e9f1c4e
fwibrahim <ibrahim@futureworkshops.com>
2017-04-06 17:14:20 +0200

    [feature:142109931]Remove categories from schema and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,22 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170330090702) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "category_translations", force: :cascade do |t|
-    t.integer  "category_id", null: false
-    t.string   "locale",      null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "name"
-    t.index ["category_id"], name: "index_category_translations_on_category_id"
-    t.index ["locale"], name: "index_category_translations_on_locale"
-  end
-
   create_table "channel_translations", force: :cascade do |t|
     t.integer  "channel_id", null: false
     t.string   "locale",     null: false
