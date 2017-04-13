c9869a769874146e7565612e779dbf245395032f
fwibrahim <ibrahim@futureworkshops.com>
2017-04-13 16:29:47 +0200

    [feature_142109917]Adds import csv

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -56,6 +56,17 @@ class Channel < ApplicationRecord
     "http://img.youtube.com/vi/#{videos.first.youtube_id}/0.jpg"
   end
 
+  def self.import_csv(csv_text)
+    channels = []
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |unstriped_row|
+      row = {}
+      unstriped_row.each{|k, v| row[k.strip] = v.strip}
+      channels << Channel.new(row.to_hash)
+    end
+    channels
+  end
+
   private
   def youtube_id_is_valid_on_youtube
     return if Rails.env.test? || Rails.env.development?
