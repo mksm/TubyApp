03b0795a811b2eba5550275681bd7ae633b7bb86
fwibrahim <ibrahim@futureworkshops.com>
2017-04-13 16:29:47 +0200

    [feature_142109917]Adds import csv

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -8,10 +8,21 @@ class Channel < ApplicationRecord
   translates :name
   globalize_accessors
 
+  def self.import_csv(csv_text)
+    channels = []
+    csv = CSV.parse(csv_text, :headers => true)
+    csv.each do |unstriped_row|
+      row = {}
+      unstriped_row.each{|k, v| row[k.strip] = v.strip}
+      channels << Channel.new(row.to_hash)
+    end
+    channels
+  end
+
   private
   def youtube_id_is_valid_on_youtube
     return if Rails.env.test? || Rails.env.development?
-    
+
     begin
       Yt::Channel.new(id: youtube_id).video_count
     rescue Yt::Errors::NoItems => e
