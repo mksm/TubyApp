5a3a8c1d456531d550d942ee7419c0b53b7e9eea
fwibrahim <ibrahim@futureworkshops.com>
2017-04-13 11:14:07 +0200

    [feature_142109943]Restores videos_count and channel_image_url

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -8,10 +8,20 @@ class Channel < ApplicationRecord
   translates :name
   globalize_accessors
 
+  def videos_count
+    videos.count
+  end
+
+  def channel_image_url
+    return nil if videos.count == 0
+
+    "http://img.youtube.com/vi/#{videos.first.youtube_id}/0.jpg"
+  end
+
   private
   def youtube_id_is_valid_on_youtube
     return if Rails.env.test? || Rails.env.development?
-    
+
     begin
       Yt::Channel.new(id: youtube_id).video_count
     rescue Yt::Errors::NoItems => e
